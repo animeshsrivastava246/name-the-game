0a4412116d17748d647e8b2da52db88ddcb370bf
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-09-09 22:46:22 +0530

    added add post api in ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,74 +9,75 @@ import AddPost from "./../components/modals/AddPost";
 import Link from "next/link";
 
 const Home = () => {
-	const [isAddPostOpen, setIsAddPostOpen] = useState(false);
-	const [posts, setPosts] = useState<PostType[]>([]);
-	const theme = useTheme();
+  const [isAddPostOpen, setIsAddPostOpen] = useState(false);
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const theme = useTheme();
 
-	const getPosts = async () => {
-		const resp = await fetch("/api/posts");
-		const data = await resp.json();
-		setPosts(data.data);
-	};
+  const getPosts = async () => {
+    const resp = await fetch("/api/posts");
+    const data = await resp.json();
+    setPosts(data.data);
+  };
 
-	useEffect(() => {
-		getPosts();
-	}, []);
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-	return (
-		<>
-			<AddPost
-				isOpen={isAddPostOpen}
-				onClose={() => {
-					setIsAddPostOpen(false);
-				}}
-			/>
-			<div className={styles.hero}>
-				<h1 className={styles.logo}>
-					<span>Name</span>
-					<span>The</span>
-					<span>Game</span>
-				</h1>
-				<Typography className={styles.hero_sub} variant="h6">
-					{` A community to identify games from photos or description. Seen a game?
+  return (
+    <>
+      <AddPost
+        isOpen={isAddPostOpen}
+        onClose={() => {
+          setIsAddPostOpen(false);
+        }}
+        refresh={getPosts}
+      />
+      <div className={styles.hero}>
+        <h1 className={styles.logo}>
+          <span>Name</span>
+          <span>The</span>
+          <span>Game</span>
+        </h1>
+        <Typography className={styles.hero_sub} variant="h6">
+          {` A community to identify games from photos or description. Seen a game?
           Don't remember the name? Do Not Worry Gamer We'll Tell You!`}
-				</Typography>
-			</div>
-			<div className={styles.main}>
-				<div className={styles.actions}>
-					<Typography
-						style={{
-							color: theme.palette.secondary.contrastText,
-						}}
-						variant="h2"
-					>
-						Questions:
-					</Typography>
-					<Button
-						onClick={() => setIsAddPostOpen(true)}
-						style={{
-							background: theme.palette.secondary.main,
-							color: theme.palette.secondary.contrastText,
-							fontWeight: 600,
-						}}
-					>
-						{"Let's Find Your Game!"}
-					</Button>
-				</div>
-				<div className={styles.posts_container}>
-					{posts.map((post, i) => (
-						<Link
-							href={`/${post.id}`}
-							key={post.id}
-							className={styles.post_card}
-						>
-							<PostCard post={post} />
-						</Link>
-					))}
-				</div>
-			</div>
-		</>
-	);
+        </Typography>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.actions}>
+          <Typography
+            style={{
+              color: theme.palette.secondary.contrastText,
+            }}
+            variant="h2"
+          >
+            Questions:
+          </Typography>
+          <Button
+            onClick={() => setIsAddPostOpen(true)}
+            style={{
+              background: theme.palette.secondary.main,
+              color: theme.palette.secondary.contrastText,
+              fontWeight: 600,
+            }}
+          >
+            {"Let's Find Your Game!"}
+          </Button>
+        </div>
+        <div className={styles.posts_container}>
+          {posts.map((post, i) => (
+            <Link
+              href={`/${post.id}`}
+              key={post.id}
+              className={styles.post_card}
+            >
+              <PostCard post={post} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/modals/AddPost.tsx
+++ src/components/modals/AddPost.tsx
@@ -5,14 +5,41 @@ import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
 import "./modals.css";
+import { useRef } from "react";
+import { AddPostRequestType } from "@/models/apiTypes";
 
 function AddPost({
   isOpen,
   onClose,
+  refresh,
 }: {
   isOpen: boolean;
   onClose: () => void;
+  refresh: () => void;
 }) {
+  const formRef = useRef<HTMLFormElement>(null!);
+
+  const handlePostSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const body: AddPostRequestType = {
+      title: formRef.current.post_title.value,
+      name: formRef.current.author_name.value,
+      email: formRef.current.author_email.value,
+      image: formRef.current.image_link.value,
+      links: formRef.current.other_links.value,
+      description: formRef.current.post_description.value,
+    };
+
+    const resp = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify(body),
+    }).then((res) => res.json());
+    if (resp) {
+      refresh();
+      onClose();
+    }
+  };
+
   return (
     <Dialog
       className={`modal add-post-modal`}
@@ -20,7 +47,7 @@ function AddPost({
       onClose={onClose}
       aria-labelledby={""}
     >
-      <form>
+      <form ref={formRef} onSubmit={handlePostSubmit}>
         <DialogTitle id="">Ask Your Question</DialogTitle>
         <DialogContent>
           <TextField
@@ -28,6 +55,7 @@ function AddPost({
             className="title-input"
             id=""
             label="Post Title"
+            name="post_title"
             variant="outlined"
           />
           <TextField
@@ -35,6 +63,7 @@ function AddPost({
             className="name-input"
             id=""
             label="Your Name"
+            name="author_name"
             variant="outlined"
           />
           <TextField
@@ -42,6 +71,7 @@ function AddPost({
             className="email-input"
             id=""
             label="Your Email"
+            name="author_email"
             type="email"
             variant="outlined"
           />
@@ -50,6 +80,7 @@ function AddPost({
             className="image-input"
             id=""
             label="Game Image Link"
+            name="image_link"
             variant="outlined"
           />
           <TextField
@@ -57,6 +88,7 @@ function AddPost({
             className="links-input"
             id=""
             label="Other Links (seprate by ' , ')"
+            name="other_links"
             variant="outlined"
           />
           <TextField
@@ -64,6 +96,7 @@ function AddPost({
             className="description-input"
             id=""
             label="Description"
+            name="post_description"
             multiline
             rows={4}
             variant="outlined"
