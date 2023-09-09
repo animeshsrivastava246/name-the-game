371428131efd564302c1bfc6b7af2029052d0f8f
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-09-09 22:50:53 +0530

    fixed header back button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@mui/material";
 import styles from "./header.module.css";
+import { useRouter } from "next/navigation";
 
 function Header() {
+  const router = useRouter();
+
   return (
     <div className={styles.header}>
-      <Button className={styles.back_btn}>{"<"}</Button>
+      <Button onClick={() => router.push("/")} className={styles.back_btn}>
+        {"<"}
+      </Button>
       <h1 className={styles.logo}>
         <span>Name</span>
         <span>The</span>
