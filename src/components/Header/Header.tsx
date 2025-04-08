18698c4170e462a40d6725e9d52619bffc983f51
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-04-08 19:26:51 +0530

    Change: Styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,20 @@ import styles from "./header.module.css";
 import { useRouter } from "next/navigation";
 
 function Header() {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div className={styles.header}>
-      <Button onClick={() => router.push("/")} className={styles.back_btn}>
-        {"<"}
-      </Button>
-      <h1 className={styles.logo}>
-        <span>Name</span>
-        <span>The</span>
-        <span>Game</span>
-      </h1>
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<Button onClick={() => router.push("/")} className={styles.back_btn}>
+				{"<"}
+			</Button>
+			<h1 className={styles.logo}>
+				<span>Name</span>
+				<span>The</span>
+				<span>Game</span>
+			</h1>
+		</div>
+	);
 }
 
 export default Header;
