e37b755c626d642be6d0033f2b53221d59e54da8
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-04-11 22:06:08 +0530

    Fix: Colors for visibility

--- src/components/modals/AddPost.tsx
+++ src/components/modals/AddPost.tsx
@@ -9,119 +9,125 @@ import { useRef, useState } from "react";
 import { AddPostRequestType } from "@/models/apiTypes";
 
 function AddPost({
-  isOpen,
-  onClose,
-  refresh,
+	isOpen,
+	onClose,
+	refresh,
 }: {
-  isOpen: boolean;
-  onClose: () => void;
-  refresh: () => void;
+	isOpen: boolean;
+	onClose: () => void;
+	refresh: () => void;
 }) {
-  const [description, setDescription] = useState("");
-  const formRef = useRef<HTMLFormElement>(null!);
+	const [description, setDescription] = useState("");
+	const formRef = useRef<HTMLFormElement>(null!);
 
-  const handlePostSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const body: AddPostRequestType = {
-      title: formRef.current.post_title.value,
-      name: formRef.current.author_name.value,
-      email: formRef.current.author_email.value,
-      image: formRef.current.image_link.value,
-      links: formRef.current.other_links.value,
-      description: formRef.current.post_description.value,
-    };
+	const handlePostSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const body: AddPostRequestType = {
+			title: formRef.current.post_title.value,
+			name: formRef.current.author_name.value,
+			email: formRef.current.author_email.value,
+			image: formRef.current.image_link.value,
+			links: formRef.current.other_links.value,
+			description: formRef.current.post_description.value,
+		};
 
-    const resp = await fetch("/api/posts", {
-      method: "POST",
-      body: JSON.stringify(body),
-    }).then((res) => res.json());
-    if (resp) {
-      refresh();
-      onClose();
-    }
-  };
+		const resp = await fetch("/api/posts", {
+			method: "POST",
+			body: JSON.stringify(body),
+		}).then((res) => res.json());
+		if (resp) {
+			refresh();
+			onClose();
+		}
+	};
 
-  const handleDescriptionChange = (value: string): void => {
-    setDescription(value.slice(0, 500));
-  };
+	const handleDescriptionChange = (value: string): void => {
+		setDescription(value.slice(0, 500));
+	};
 
-  return (
-    <Dialog
-      className={`modal add-post-modal`}
-      open={isOpen}
-      onClose={onClose}
-      aria-labelledby={""}
-    >
-      <form ref={formRef} onSubmit={handlePostSubmit}>
-        <DialogTitle id="">Ask Your Question</DialogTitle>
-        <DialogContent>
-          <TextField
-            color="primary"
-            className="title-input"
-            id=""
-            label="Post Title"
-            name="post_title"
-            variant="outlined"
-            required
-          />
-          <TextField
-            color="primary"
-            className="name-input"
-            id=""
-            label="Your Name"
-            name="author_name"
-            variant="outlined"
-            required
-          />
-          <TextField
-            color="primary"
-            className="email-input"
-            id=""
-            label="Your Email"
-            name="author_email"
-            type="email"
-            variant="outlined"
-            required
-          />
-          <TextField
-            color="primary"
-            className="image-input"
-            id=""
-            label="Game Image Link"
-            name="image_link"
-            variant="outlined"
-          />
-          <TextField
-            color="primary"
-            className="links-input"
-            id=""
-            label="Other Links (seprate by ' , ')"
-            name="other_links"
-            variant="outlined"
-          />
-          <TextField
-            color="primary"
-            className="description-input"
-            id=""
-            label="Description"
-            name="post_description"
-            multiline
-            rows={4}
-            value={description}
-            onChange={(e) => handleDescriptionChange(e.target.value)}
-            variant="outlined"
-            required
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button type="submit">{"Let's Find Your Game!"}</Button>
-          <Button onClick={onClose} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
-  );
+	return (
+		<Dialog
+			className={`modal add-post-modal`}
+			open={isOpen}
+			onClose={onClose}
+			aria-labelledby={""}
+		>
+			<form ref={formRef} onSubmit={handlePostSubmit}>
+				<DialogTitle id="">Ask Your Question</DialogTitle>
+				<DialogContent>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="title-input"
+						label="Post Title"
+						name="post_title"
+						variant="outlined"
+						required
+					/>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="name-input"
+						label="Your Name"
+						name="author_name"
+						variant="outlined"
+						required
+					/>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="email-input"
+						label="Your Email"
+						name="author_email"
+						type="email"
+						variant="outlined"
+						required
+					/>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="image-input"
+						label="Game Image Link"
+						name="image_link"
+						variant="outlined"
+					/>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="links-input"
+						label="Other Links (separate by ',')"
+						name="other_links"
+						variant="outlined"
+					/>
+					<TextField
+						fullWidth
+						margin="dense"
+						color="primary"
+						className="description-input"
+						label="Description"
+						name="post_description"
+						multiline
+						rows={4}
+						value={description}
+						onChange={(e) => handleDescriptionChange(e.target.value)}
+						variant="outlined"
+						required
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button type="submit">{"Let's Find Your Game!"}</Button>
+					<Button onClick={onClose} color="primary">
+						Cancel
+					</Button>
+				</DialogActions>
+			</form>
+		</Dialog>
+	);
 }
 
 export default AddPost;
