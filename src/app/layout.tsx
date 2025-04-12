48630b2ace2212a97b0b700888ba8bd9198e27c3
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-04-12 22:10:00 +0530

    Add: Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 "use client";
 import { ThemeProvider } from "@emotion/react";
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import theme from "./muiTheme";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "NameTheGame",
+	description: "A community to identify games from photos or description.",
+	metadataBase: new URL("https://name-the-game.vercel.app/"),
+};
 
 export default function RootLayout({
 	children,
